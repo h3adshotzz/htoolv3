e13cfd92094797a3c82878fcd4689d35bec4d6cc
Harry Moulton <h3@Harrys-MacBook-Pro.local>
2023-02-23 23:56:37 +0000

    Libhelper: bump libhelper

--- include/htool-version.h
+++ include/htool-version.h
@@ -27,9 +27,9 @@
 
 /* htool Source Version, i.e. 312.56.12 */
 #define HTOOL_SOURCE_VERSION_MAJOR      "14"
-#define HTOOL_SOURCE_VERSION_MINOR      "27"
+#define HTOOL_SOURCE_VERSION_MINOR      "28"
 
-#define HTOOL_SOURCE_VERSION            "htool-301.14.27"
+#define HTOOL_SOURCE_VERSION            "htool-301.14.28"
 
 /**
  *  Define a Target type for HTool to both display within the help
