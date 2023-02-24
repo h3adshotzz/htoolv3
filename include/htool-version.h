473891c8d1ac6fbc2bad9a4fccb19c8768f9cf5f
Harry Moulton <h3@Harrys-MBP.localdomain>
2023-02-24 12:13:56 +0000

    CI: TEST: Bump htool version to trigger ci

--- include/htool-version.h
+++ include/htool-version.h
@@ -27,9 +27,9 @@
 
 /* htool Source Version, i.e. 312.56.12 */
 #define HTOOL_SOURCE_VERSION_MAJOR      "14"
-#define HTOOL_SOURCE_VERSION_MINOR      "28"
+#define HTOOL_SOURCE_VERSION_MINOR      "29"
 
-#define HTOOL_SOURCE_VERSION            "htool-301.14.28"
+#define HTOOL_SOURCE_VERSION            "htool-301.14.29"
 
 /**
  *  Define a Target type for HTool to both display within the help
